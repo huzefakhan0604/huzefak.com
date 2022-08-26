eee3ad91431bbc29a9e65948d3c144b0e348c45e
huzefakhan0604 <112191335+huzefakhan0604@users.noreply.github.com>
2022-08-26 12:07:21 -0400

    Add files via upload

---
+++ huzefa.js
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>
+      User Registration Form
+    </title>
+    <style>
+      h1{
+      background-color: pink;
+      font-style: italic;
+      text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <h1> User Registration Form</h1>
+    <form>
+      Name <input type="text" name="form_name"><br><br>
+      Age <input type="number" name="form_age"><br><br>
+      Email ID <input type="email" name="form_email"><br><br>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
